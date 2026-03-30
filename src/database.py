335c9ad0872748008e71e9ee9aa500ddef9bb5b0
Mohamed <momoai.learn@example.com>
2026-03-30 12:01:32 +0200

    Created database.py

---
+++ src/database.py
@@ -0,0 +1,22 @@
+import sqlite3
+from pathlib import Path
+
+def get_db_path() -> Path:
+    # Resolves the absolute path to the database file dynamically
+    current_dir = Path(__file__).parent
+    project_root = current_dir.parent
+    
+    return project_root / "data" / "vivino.db"
+
+def execute_sql_file(sql_file_path: str) -> list:
+    db_path = get_db_path()
+
+    with open(sql_file_path, 'r') as file:
+        query = file.read()
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+    return results
