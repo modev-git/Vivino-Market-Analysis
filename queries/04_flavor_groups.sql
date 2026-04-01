d6a1165f8a2c13fb5489d81f7ef8560ef1a3795a
Mohamed <momoai.learn@example.com>
2026-04-01 21:04:07 +0200

    finalized 4th question

---
+++ queries/04_flavor_groups.sql
@@ -0,0 +1,14 @@
+SELECT wines.name, 
+	keywords.name AS flavor_name, 
+	SUM(keywords_wine.count) AS user_flavor_count,
+	keywords_wine.group_name
+FROM wines
+JOIN keywords_wine
+	ON keywords_wine.wine_id = wines.id
+JOIN keywords
+	ON keywords.id = keywords_wine.keyword_id
+WHERE keywords.name IN ('coffee', 'toast', 'green apple', 'cream', 'citrus')
+GROUP BY wines.name, keywords.name, keywords_wine.group_name
+HAVING SUM(keywords_wine.count) > 10
+
+
